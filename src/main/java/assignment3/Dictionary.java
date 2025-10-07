da5f3ee85bed9ed43895343d4bb7d5e812ee62c0
ah-ns <htvanlierop@loyola.edu>
2025-10-07 13:27:33 -0400

    random word generation and guess validation

--- src/main/java/assignment3/Dictionary.java
+++ src/main/java/assignment3/Dictionary.java
@@ -1,14 +1,52 @@
+/**
+ * Purpose: Generate random words from a list, check if guesses are valid
+ * 
+ * @author Hans van Lierop
+ * @version 1
+ */
 package assignment3;
 
 import java.time.LocalTime;
 
 public class Dictionary {
 
+	String[] words = {
+			"apple", "these", "super", "paint", "clean",
+			"jesty", "apply", "wrong", "curry", "drain",
+			"slain", "sword", "irate", "crazy", "close"
+	};
+	
+	public Dictionary() {
+		
+	}
+	
+	/**
+	 * Determine if a word is a valid guess
+	 * 
+	 * @param word The word to be checked
+	 * @return true if the word is valid, false if not
+	 */
 	public boolean isValidWord(String word) {
+		// Make sure each character of the word is a proper letter
+		int lower = 65;
+		int upper = 122;
+		for (int i = 0; i < word.length(); i++) {
+			char current = word.charAt(i);
+			if ((int) current < lower | (int) current > upper)
+				return false;
+		}
 		return true;
 	}
 	
+	/**
+	 * Get random word from words array
+	 * 
+	 * @return Random string from the array
+	 */
 	public String getRandomWord() {
-		return "";
+		// Generate random number from current time
+		int randomNum = LocalTime.now().getSecond() % (this.words.length);
+		// Return element at the random index
+		return this.words[randomNum];
 	}
 }
